0a17b58b53e1b0434d0a45ca2c48dc231e525f03
miavai649 <mahmudulhaquenoor@gmail.com>
2024-03-02 18:50:28 +0600

    top rated page done

--- src/pages/TopRated.js
+++ src/pages/TopRated.js
@@ -30,7 +30,7 @@ const TopRated = () => {
   }
   return (
     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
-      this is top rated page
+      {content}
     </div>
   )
 }
